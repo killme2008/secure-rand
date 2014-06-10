5048ad740624eb50803df447f587dfe397d8af7f
dennis zhuang <killme2008@gmail.com>
2014-06-10 23:39:27 +0800

    Updated version

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject secure-rand "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject secure-rand "0.1"
+  :description "A Clojure library designed to generate secure random float,int,bytes and strings based on java.security.SecureRandom."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
